860f7cda6aad897977fd09d4b48664d105024a2f
alikhairi <63368949+alikhairi@users.noreply.github.com>
2020-04-28 01:20:51 +0800

    Update node.py

--- fireeyeminer/node.py
+++ fireeyeminer/node.py
@@ -15,23 +15,46 @@ class Miner(BasePollerFT):
     def configure(self):
         super(Miner, self).configure()
         
+        self.public_key = None
+        self.private = None
+        self.numdays = None
+        
         self.polling_timeout = self.config.get('polling_timeout', 20)
         self.verify_cert = self.config.get('verify_cert', True)
-        
-        self.public_key = self.config.get('public_key', None)
-        if self.public_key is None:
-            raise ValueError('%s - Public Key is required' % self.name)
-        
-        self.private_key = self.config.get('private_key', None)
-        if self.private_key is None:
-            raise ValueError('%s - Private Key is required' % self.name)
-        
-        self.numdays = self.config.get('numdays', None)
-        if self.numdays is None:
-            raise ValueError('%s - # of Days is required' % self.name)
-        
         self.url = 'api.isightpartners.com'
         self.indicators = 'ip,sha256,url,domain'
+        
+        self.side_config_path = self.config.get('side_config', None)
+        if self.side_config_path is None:
+            self.side_config_path = os.path.join(
+                os.environ['MM_CONFIG_DIR'],
+                '%s_side_config.yml' % self.name
+            )        
+        
+        self._load_side_config()
+        
+        
+    def _load_side_config(self):
+        try:
+            with open(self.side_config_path, 'r') as f:
+                sconfig = yaml.safe_load(f)
+
+        except Exception as e:
+            LOG.error('%s - Error loading side config: %s', self.name, str(e))
+            return
+
+        self.public_key = sconfig.get('public_key', None)
+        if self.public_key is not None:
+            LOG.info('%s - Public key set', self.name)
+
+        self.private_key = sconfig.get('private_key', None)
+        if self.private_key is not None:
+            LOG.info('%s - Private key set', self.name)
+        
+        self.numdays = sconfig.get('numdays', None)
+        if self.numdays is not None:
+            LOG.info('%s - number of day set', self.name)            
+         
 
         
     def _process_item(self, item):
@@ -76,3 +99,26 @@ class Miner(BasePollerFT):
         data = json.loads(response.read())
         conn.close()
         return data
+    
+    def hup(self, source=None):
+        LOG.info('%s - hup received, reload side config', self.name)
+        self._load_side_config()
+        super(ExportList, self).hup(source=source)
+
+    @staticmethod
+    def gc(name, config=None):
+        basepoller.BasePollerFT.gc(name, config=config)
+
+        side_config_path = None
+        if config is not None:
+            side_config_path = config.get('side_config', None)
+        if side_config_path is None:
+            side_config_path = os.path.join(
+                os.environ['MM_CONFIG_DIR'],
+                '{}_side_config.yml'.format(name)
+            )
+
+        try:
+            os.remove(side_config_path)
+        except:
+            pass
