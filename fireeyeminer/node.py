1ea0b987c0671bb86ee7e2678809572f65bc83af
alikhairi <63368949+alikhairi@users.noreply.github.com>
2020-04-28 02:27:27 +0800

    Update node.py

--- fireeyeminer/node.py
+++ fireeyeminer/node.py
@@ -13,15 +13,15 @@ LOG = logging.getLogger(__name__)
 
 class Miner(BasePollerFT):
     def configure(self):
-        super(Miner, self).configure()        
-   
+        super(Miner, self).configure()
+
         self.polling_timeout = self.config.get('polling_timeout', 20)
         self.verify_cert = self.config.get('verify_cert', True)
-        
+
         self.public_key = self.config.get('public_key', None)
         if self.public_key is None:
             raise ValueError('%s - Public key is required' % self.name)
-        
+
         self.private_key = self.config.get('private_key', None)
         if self.private_key is None:
             raise ValueError('%s - Private key is required' % self.name)
@@ -29,48 +29,53 @@ class Miner(BasePollerFT):
         self.numdays = self.config.get('numdays', None)
         if self.numdays is None:
             raise ValueError('%s - Number of days is required' % self.name)
-        
+
         self.url = 'api.isightpartners.com'
         self.indicators = 'ip,sha256,url,domain'
-        
-   def _process_item(self, item):
-        indicators = self.indicators.split(',')
-        iocs = {}
-        for indicator in indicators:
-            for message in item['message']:
-                if message[indicator]:
-                    if indicator in iocs:
-                        iocs[indicator].append(message[indicator])
-                    else:
-                        iocs[indicator] = [message[indicator]]
-        for ioc in iocs:
-            if ioc == 'ip':
-                value = {'type': 'IPv4', 'confidence': 100}
-            else:
-                value = {'type': ioc, 'confidence': 100}
-            return [[iocs[ioc], value]]
-        
-        
-    def _build_iterator(self, item):
-        start = int(time.time()) - (86400 * self.numdays)
-        end = int(time.time())
-        search_query = '/view/iocs?startDate=' + str(start) + '&endDate=' + str(end) + '&indicatorTypes=' + self.indicators
-        accept_version = '2.2'
-        accept_header = 'application/json'
-        time_stamp = email.Utils.formatdate(localtime=True)
-        hash_data = search_query + accept_version + accept_header + time_stamp
-        hashed = hmac.new(self.private_key, hash_data, hashlib.sha256)
-        headers = {
-            'Accept': accept_header,
-            'Accept-Version': accept_version,
-            'X-Auth': self.public_key,
-            'X-Auth-Hash': hashed.hexdigest(),
-            'Date': time_stamp,
-        }
-        conn = httplib.HTTPSConnection(self.url)
-        conn.request('GET', search_query, '', headers)
-        response = conn.getresponse()
-        data = json.loads(response.read())
-        conn.close()
-        return data
-    
+
+
+def _process_item(self, item):
+    value = {'type': 'IPv4', 'confidence': 100}
+    return [[item, value]]
+
+
+def _build_iterator(self, item):
+    start = int(time.time()) - (86400 * self.numdays)
+    end = int(time.time())
+    search_query = '/view/iocs?startDate=' + str(start) + '&endDate=' + str(end) + '&indicatorTypes=' + self.indicators
+    accept_version = '2.2'
+    accept_header = 'application/json'
+    time_stamp = email.Utils.formatdate(localtime=True)
+    hash_data = search_query + accept_version + accept_header + time_stamp
+    hashed = hmac.new(self.private_key, hash_data, hashlib.sha256)
+    headers = {
+        'Accept': accept_header,
+        'Accept-Version': accept_version,
+        'X-Auth': self.public_key,
+        'X-Auth-Hash': hashed.hexdigest(),
+        'Date': time_stamp,
+    }
+    conn = httplib.HTTPSConnection(self.url)
+    conn.request('GET', search_query, '', headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+    conn.close()
+
+    indicators = self.indicators.split(',')
+    iocs = {}
+    for indicator in indicators:
+        for message in data['message']:
+            if message[indicator]:
+                if indicator in iocs:
+                    iocs[indicator].append(message[indicator])
+                else:
+                    iocs[indicator] = [message[indicator]]
+    return iocs['ip']
+    '''
+    for ioc in iocs:
+        if ioc == 'ip':
+            value = {'type': 'IPv4', 'confidence': 100}
+        else:
+            value = {'type': ioc, 'confidence': 100}
+        return [[iocs[ioc], value]]
+    '''
